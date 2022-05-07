baeba8fa90d33cfea85927cc8f2659f81b303c7f
Ian Hulsbus <ian@hulsbus.be>
2022-05-07 20:51:56 +0200

    Build fix attempt

---
+++ src/environments/set-env.ts
@@ -0,0 +1,31 @@
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+  // Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+  // Load node modules
+  const appVersion = require('../../package.json').version;
+  // require('dotenv').config({
+  //   path: 'src/environments/.env'
+  // });
+  // `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  authority: '${process.env?.['AUTHORITY']}',
+  clientId: '${process.env?.['CLIENTID']}',
+  baseURL: '${process.env?.['BASEURL']}',
+  backend: '${process.env?.['BACKEND']}',
+  production: true,
+};
+`;
+  console.log('The file `environment.ts` will be written with the following content: \n');
+  writeFile(targetPath, envConfigFile, (err: any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+    }
+  });
+};
+
+setEnv();
